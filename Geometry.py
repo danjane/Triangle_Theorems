3c7fc507f0d642895574d032de412a95b8ca1104
danjane <jdtjane@gmail.com>
2017-10-23 23:56:59 +0200

    Big code change to try to avoid such a horrendous explosion of 'interesting' constructions. First step was to change how the objects referenced each other (which was bad).
    For now still in pretty much the same place, but by rethinking 'tasks' as 'potential parents' I'm hoping it will be easier to choose routes through the constructions.

--- Geometry.py
+++ Geometry.py
@@ -1,41 +1,17 @@
 import numpy as np
 import pylab
 
-class Task:
-    """A geometric construction."""
-
-    def __init__(self, obj1, obj2):
-        """Initializes the data."""
-        self.obj1 = obj1
-        self.obj2 = obj2
-        self.doable = False
-
-        if (obj1.name == 'Point') and (obj2.name == 'Point'):
-            self.type = 'p2p'
-            self.text = "Join two points with a line ({} and {})".format(id(obj1), id(obj2))
-            self.doable = True
-
-        if (obj1.name == 'Line') and (obj2.name == 'Line'):
-            self.type = 'lx'
-            self.text = "Find point of intersection ({} and {})".format(id(obj1), id(obj2))
-            self.doable = True
-
-        if (obj1.name == 'Point') and (obj2.name == 'Angle') and (obj2.point == obj1):
-            self.type = 'tri'
-            self.text = "At point, find trisector ({} and {})".format(id(obj1), id(obj2))
-            self.doable = True
-
 
 class GeometricCollection:
     """Represents a collection of geometric objects."""
 
     def __init__(self):
         """Initializes the data."""
-        self.population = []
-        self.tasks = []
-        self.data = []
-
-        print("(Initializing GC = {})".format(id(self)))
+        self.population = 0
+        self.objects = []
+        self.parents = {}
+        self.data = {}
+        self.tasks = set()  # potential parents
 
     def point(self, *arg, **kw):
         x = Point(*arg, **kw)
@@ -49,86 +25,94 @@ class GeometricCollection:
 
     def angle(self, *arg, **kw):
         x = Angle(*arg, **kw)
-        self.add_obj(x)
-        self.data.append(np.rad2deg(x.theta))
-
+        x = self.add_obj(x)
+        self.tasks.add((x.number,))
+        self.data[x.number] = np.rad2deg(x.theta)
         # Also add complement angle
-        x = Angle(x.theta + x.alpha - np.pi, x.alpha, x.point) #hmmm... clearly needs a think
-        self.add_obj(x)
-        self.data.append(x.theta)
-
+        # x = Angle(x.theta + x.alpha - np.pi, x.alpha, x.point) #hmmm... clearly needs a think
+        # self.add_obj(x)
+        # self.data.append(x.theta)
         return x
 
     def add_obj(self, new_obj):
-        for obj in self.population:
-            task = Task(obj, new_obj)
-            if task.doable:
-                self.tasks.append(task)
+        new_num = self.population
+        new_obj.number = new_num
 
-        self.population.append(new_obj)
-
-    def get_tasks(self, task_type):
-        new_tasks = []
-        now_tasks = []
-        for task in self.tasks:
-            if task.type == task_type:
-                now_tasks.append(task)
+        # This new object can have new kids!
+        for old_num in range(new_num):
+            if ((old_num - new_num) % 3) == 2:
+                self.tasks.add((new_num, old_num))
             else:
-                new_tasks.append(task)
-        self.tasks = new_tasks
-        return now_tasks
+                self.tasks.add((old_num, new_num))
 
-    def connect_points(self):
-        now_tasks = self.get_tasks('p2p')
-        for task in now_tasks:
-            p = task.obj1
-            q = task.obj2
-            dy = p.y-q.y
-            dx = p.x-q.x
+        self.objects.append(new_obj)
+        self.population += 1
+        return new_obj
 
-            self.line(p.x, p.y, np.arctan2(dy, dx), [p, q])
+    def connect_points(self, p, q):
+        dy = p.y-q.y
+        dx = p.x-q.x
+        l = self.line(p.x, p.y, np.arctan2(dy, dx), [p, q])
+        self.data[l.number] = np.sqrt(dx**2 + dy**2)
 
-            self.data.append(np.sqrt(dx**2 + dy**2))
+    def intersect_lines(self, l1, l2):
+        p = set(l1.parents).intersection(l2.parents)
 
-    def intersect_lines(self):
-        now_tasks = self.get_tasks('lx')
-        for task in now_tasks:
-            l1 = task.obj1
-            l2 = task.obj2
+        if p:
+            p = p.pop()
+            p = self.objects[p]
 
-            p = set(l1.points).intersection(l2.points)
+        else:
 
-            if p:
-                p = p.pop()
+            # Check for parallel lines?
+            if l1.theta == l2.theta:
+                if l1.theta == np.arctan2(l1.y - l2.y, l1.x - l2.x):
+                    print("Lines are equal!! ({} and {})".format(id(l1), id(l2)))
+                else:
+                    print("Lines are parallel!! ({} and {})".format(id(l1), id(l2)))
 
-            else:
+            c1 = np.tan(l1.theta)
+            c2 = np.tan(l2.theta)
 
-                # Check for parallel lines?
-                if l1.theta == l2.theta:
-                    if l1.theta == np.arctan2(l1.y-l2.y, l1.x-l2.x):
-                        print("Lines are equal!! ({} and {})".format(id(l1), id(l2)))
-                    else:
-                        print("Lines are parallel!! ({} and {})".format(id(l1), id(l2)))
+            x = (l2.y - l1.y - c2 * l2.x + c1 * l1.x) / (c1 - c2)
+            y = (c1 * l2.y - c2 * l1.y - c1 * c2 * l2.x + c1 * c2 * l1.x) / (c1 - c2)
 
-                c1 = np.tan(l1.theta)
-                c2 = np.tan(l2.theta)
+            p = self.point(x, y, [l1, l2])
 
-                x = (l2.y - l1.y - c2 * l2.x + c1 * l1.x) / (c1 - c2)
-                y = (c1*l2.y - c2*l1.y - c1*c2 * l2.x + c1*c2 * l1.x) / (c1 - c2)
+        # Now create an angle from this intersection
+        self.angle(l1, l2, p)
 
-                p = self.point(x, y, [l1, l2])
+    def trisect_angle(self, angle):
+        p = angle.point
+        self.line(p.x, p.y, angle.alpha + angle.theta/3., [p])
+        self.line(p.x, p.y, angle.alpha + angle.theta*2./3., [p])
 
-            # Now create an angle from this intersection
-            self.angle(l1.theta, l2.theta, p)
+    def do_all_tasks(self):
+        current_tasks = set(self.tasks)
+        self.tasks = set()
+        for task in current_tasks:
+            self.do_task(task)
 
-    def trisect_angles(self):
-        now_tasks = self.get_tasks('tri')
-        for task in now_tasks:
-            p = task.obj1
-            angle = task.obj2
+    def do_task(self, parents):
+        if len(parents) == 1:
+            self.do_task1(parents)
+        elif len(parents) == 2:
+            self.do_task2(parents)
+        else:
+            print("something gone wrong!!\n")
 
-            self.line(p.x, p.y, angle.alpha + angle.theta/3., [p])
-            self.line(p.x, p.y, angle.alpha + angle.theta*2./3., [p])
+    def do_task2(self, parents):
+        obj1 = self.objects[parents[0]]
+        obj2 = self.objects[parents[1]]
+        if (obj1.name == 'Point') and (obj2.name == 'Point'):
+            self.connect_points(obj1, obj2)
+        elif (obj1.name == 'Line') and (obj2.name == 'Line'):
+            self.intersect_lines(obj1, obj2)
+
+    def do_task1(self, parents):
+        obj1 = self.objects[parents[0]]
+        if obj1.name == 'Angle':
+            self.trisect_angle(obj1)
 
     def what_tasks(self):
         """Prints the current population."""
@@ -142,13 +126,13 @@ class GeometricCollection:
 
     def plot_constructions(self):
 
-        for obj in reversed(self.population):
+        for obj in reversed(self.objects):
             obj.plot()
 
-        #pylab.title('Probability distribution of anharmonic oscillator with beta=' + str(beta))
-        #pylab.xlabel('position')
-        #pylab.ylabel('probability')
-        #pylab.legend(['matrix squaring', 'path sampled'])
+        # pylab.title('Probability distribution of anharmonic oscillator with beta=' + str(beta))
+        # pylab.xlabel('position')
+        # pylab.ylabel('probability')
+        # pylab.legend(['matrix squaring', 'path sampled'])
         pylab.xlim(-1., 2.)
         pylab.ylim(-1., 2.)
         pylab.show()
@@ -161,6 +145,8 @@ class Geometric:
     def __init__(self, name):
         """Initializes the data."""
         self.name = name
+        self.parents = []
+        self.number = -1
         print("(Initializing {})".format(self.name))
 
     def plot(self):
@@ -176,7 +162,7 @@ class Point(Geometric):
         self.y = y
         if lines is None:
             lines = []
-        self.lines = lines
+        self.parents = lines
         print("Point x:{}, y:{}:".format(x, y))
 
     def plot(self):
@@ -193,7 +179,7 @@ class Line(Geometric):
         self.theta = theta
         if points is None:
             points = []
-        self.points = points
+        self.parents = [p.number for p in points]
 
     def plot(self):
         pylab.plot(
@@ -205,16 +191,16 @@ class Line(Geometric):
 class Angle(Geometric):
     """Represents an angle theta (inclined at angle alpha) at a point."""
 
-    def __init__(self, angle1, angle2, point):
+    def __init__(self, line1, line2, point):
         Geometric.__init__(self, 'Angle')
 
-        # TODO want the angle to point into the triangle if possible
-        theta = angle1-angle2
-        alpha = angle2
+        theta = line1.theta-line2.theta - np.pi
+        alpha = line2.theta
 
-        self.theta = theta
-        self.alpha = alpha
+        self.theta = theta % (2*np.pi)
+        self.alpha = alpha % (2*np.pi)
         self.point = point
+        self.parents = [line1.number, line2.number]
 
     def plot(self):
         p = self.point
@@ -228,12 +214,12 @@ triangle = GeometricCollection()
 triangle.point(0., 0.)
 triangle.point(1., 0.1)
 triangle.point(1.1, 1.)
-triangle.connect_points()
-triangle.intersect_lines()
-triangle.trisect_angles()
-triangle.intersect_lines()
-triangle.connect_points()
-# triangle.what_tasks()
+triangle.do_all_tasks()
+triangle.do_all_tasks()
+triangle.do_all_tasks()
+triangle.do_all_tasks()
+
 triangle.show_data()
+print len(triangle.tasks)
 print min([abs(i-60) for i in triangle.data])
-#triangle.plot_constructions()
+triangle.plot_constructions()
